58988c6c5da2fa91c7e7747b2334cb961230f0f8
Xinwei Wang <xinwei.wang@appfolio.com>
2020-05-21 13:38:36 -0700

    xww - Create migration to create image table

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_20_193312) do
+
+  create_table "images", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "url", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
